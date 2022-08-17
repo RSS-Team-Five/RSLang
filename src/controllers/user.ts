2516507b7506c0170a9ce900eb12af5607572eb9
muannna <evagoeshard@gmail.com>
2022-08-18 01:51:15 +0500

    feat: add user contoller and user api methods

---
+++ src/controllers/user.ts
@@ -0,0 +1,58 @@
+import { createUser, getUser, getRefreshToken, signIn, updateUser, deleteUser } from "../api/users/usersApi";
+
+export default class User {
+  name: string;
+
+  email: string;
+
+  password: string;
+
+  user: {};
+
+  constructor({ name, email, password }: { name: string; email: string; password: string }) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.user = {
+      name,
+      email,
+      password,
+    }
+  }
+
+  async createUser({ name, email, password }: { name: string; email: string; password: string }) {
+    const user = await createUser({ name, email, password });
+    this.user = user;
+    return this.user;
+  }
+
+  async signInUser({ email, password }: { email: string; password: string }) {
+    const user = await signIn({ email, password })
+    this.user = Object.assign(this.user, user);
+    return this.user;
+  }
+
+  async getUser({ userId, token }: { userId: string; token: string }) {
+    const user = await getUser({ userId, token });
+    this.user = Object.assign(this.user, user);
+    return this.user;
+  }
+
+  async getToken({ userId, refreshToken }: { userId: string; refreshToken: string }) {
+    const user = await getRefreshToken({ userId, refreshToken });
+    this.user = Object.assign(this.user, user);
+    return this.user;
+  }
+
+  async updateUser({ email, password }: { email: string; password: string }, userId: string, token: string) {
+    const user = await updateUser({ email, password, userId, token })
+    this.user = Object.assign(this.user, user);
+    return this.user;
+  }
+
+  async deleteUser({ userId, token }: { userId: string; token: string }) {
+    const user = await deleteUser({ userId, token });
+    this.user = Object.assign(this.user, user);
+    return this.user;
+  }
+}
