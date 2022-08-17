2689c5cfe41e5c070dee9d418772ffb09b6d15e4
muannna <evagoeshard@gmail.com>
2022-08-18 01:51:15 +0500

    feat: add user contoller and user api methods

---
+++ src/api/users/usersApi.ts
@@ -0,0 +1,106 @@
+const hostBase = 'http://host1836051.hostland.pro';
+const users = `${hostBase}/users`;
+const sign = `${hostBase}/signin`;
+
+export const createUser = async ({ name, email, password }: { name: string; email: string; password: string }) => {
+  try {
+    const response = await fetch(`${users}`, {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    return { isSuccess: false }; // Incorrect e-mail or password
+  }
+}
+
+export const signIn = async ({ email, password }: { email: string; password: string }) => {
+  try {
+    const response = await fetch(`${sign}`, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    return { isSuccess: false }; // Incorrect e-mail or password
+  }
+}
+
+export const getUser = async ({ userId, token }: { userId: string; token: string }) => {
+  try {
+    const response = await fetch(`${users}/${userId}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    return { isSuccess: false };
+  }
+}
+
+export const updateUser = async ({ email, password, userId, token }: { email: string; password: string, userId: string, token: string }) => {
+  try {
+    const response = await fetch(`${users}/${userId}`, {
+      method: 'PUT',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    return { isSuccess: false };
+  }
+}
+
+export const deleteUser = async ({ userId, token }: { userId: string, token: string }) => {
+  try {
+    const response = await fetch(`${users}/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    return { isSuccess: false };
+  }
+}
+
+export const getRefreshToken = async ({ userId, refreshToken }: { userId: string; refreshToken: string }) => {
+  try {
+    const response = await fetch(`${users}/${userId}/tokens`, {
+      method: 'GET',
+      body: JSON.stringify({ refreshToken }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    return { isSuccess: false }; // Access token is missing, expired or invalid
+  }
+}
