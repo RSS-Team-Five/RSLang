93fc87603ed07c3b797861518ad4d36772f90702
Bumble <bumble.sakh@gmail.com>
2022-08-18 18:24:34 +1100

    feat: add State model

---
+++ src/models/State.ts
@@ -0,0 +1,22 @@
+const state = {
+  API: {
+    URL: 'http://host1836051.hostland.pro',
+    ENDPOINTS: {
+      USERS: 'users',
+      SIGNIN: 'signin',
+      WORDS: 'words',
+    },
+    QUERIES: {
+      WORDS: {
+        GROUP: 'group',
+        PAGE: 'page',
+      },
+    },
+  },
+  book: {
+    maxGroup: 6,
+    maxPage: 30,
+  },
+};
+
+export default state;
