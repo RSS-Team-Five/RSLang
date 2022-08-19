1604fc6bb9928de813dec1912326ff9fdd3b5fab
YuliyaShu <juliette7373@gmail.com>
2022-08-19 17:41:47 +0200

    fix: linter mistakes

--- src/views/components/footer.ts
+++ src/views/components/footer.ts
@@ -1,53 +1,49 @@
-import CustomElement from "../../utils/customElement";
+import CustomElement from '../../utils/customElement';
 import logoRSPath from '../../assets/icons/rs_school_js.svg';
 
 function footer(): HTMLElement {
-  const footerElement = new CustomElement('footer',
-  {
+  const footerElement = new CustomElement('footer', {
     className: 'footer',
   });
 
-  const container = new CustomElement('div',
-  {
+  const container = new CustomElement('div', {
     className: 'footer__container container',
   });
   footerElement.addChildren([container.element]);
 
-  const footerWrapper = new CustomElement('div',
-  {
-    className: 'footer__wrapper'
+  const footerWrapper = new CustomElement('div', {
+    className: 'footer__wrapper',
   });
   container.addChildren([footerWrapper.element]);
 
   const teamMembers = [
-    {memberName: 'Igor',  memberGithub: "Bumble-sakh"},
-    {memberName: 'Anna',  memberGithub: "muannna"},
-    {memberName: 'Yuliya',  memberGithub: "YuliyaShu"}
-  ]
+    { memberName: 'Igor', memberGithub: 'Bumble-sakh' },
+    { memberName: 'Anna', memberGithub: 'muannna' },
+    { memberName: 'Yuliya', memberGithub: 'YuliyaShu' },
+  ];
 
-  const membersLink = teamMembers.map(elem => new CustomElement('a',
-  {
-    className: 'footer__links',
-    innerText: elem.memberName,
-    href: `https://github.com/${elem.memberGithub}`
-  }).element)
+  const membersLink = teamMembers.map(
+    (elem) =>
+      new CustomElement('a', {
+        className: 'footer__links',
+        innerText: elem.memberName,
+        href: `https://github.com/${elem.memberGithub}`,
+      }).element
+  );
 
-  const dateOfProject = new CustomElement('p',
-  {
+  const dateOfProject = new CustomElement('p', {
     className: 'footer__links',
-    innerHTML: 'august 2022'
-  })
-
-  const linkToRS = new CustomElement('a',
-  {
-    className: 'footer__links',
-    href: 'https://rs.school/js/'
+    innerHTML: 'august 2022',
   });
 
-  const logoRS = new CustomElement('img',
-  {
+  const linkToRS = new CustomElement('a', {
+    className: 'footer__links',
+    href: 'https://rs.school/js/',
+  });
+
+  const logoRS = new CustomElement('img', {
     src: logoRSPath,
-    alt: 'rs-logo'
+    alt: 'rs-logo',
   });
   linkToRS.addChildren([logoRS.element]);
   footerWrapper.addChildren([...membersLink, dateOfProject.element, linkToRS.element]);
