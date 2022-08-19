d5ab725aa0c8ee8e05d61a46796aff4da45c3cfb
YuliyaShu <juliette7373@gmail.com>
2022-08-19 20:26:36 +0200

    fix: words api path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'normalize.css';
-import { oneWordFromAPI, wordsFromAPI } from './api/words/wordsApi';
 import App from './controllers/App';
 import state from './models/State';
 import './scss/index.scss';
@@ -7,5 +6,3 @@ import './scss/index.scss';
 const app = new App(state);
 
 app.start();
-console.log(wordsFromAPI());
-console.log(oneWordFromAPI('5e9f5ee35eb9e72bc21b005a'));
