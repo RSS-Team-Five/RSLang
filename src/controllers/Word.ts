125383b5f3533ff6e5b9e83853c3b417cf4a34fc
Bumble <bumble.sakh@gmail.com>
2022-08-20 00:58:04 +1100

    fix: files_for_prettier

--- src/controllers/Word.ts
+++ src/controllers/Word.ts
@@ -1,15 +1,15 @@
-import { oneWordFromAPI } from "../api/words/wordsApi";
-import IWord from "../types/IWord";
+import { oneWordFromAPI } from '../api/words/wordsApi';
+import IWord from '../types/IWord';
 
 class Word {
   id: string;
 
-  constructor(id: string, ) {
+  constructor(id: string) {
     this.id = id;
   }
 
   async getOneWord(): Promise<IWord> {
-    const word = await oneWordFromAPI(this.id) as IWord;
+    const word = (await oneWordFromAPI(this.id)) as IWord;
     return word;
   }
 }
