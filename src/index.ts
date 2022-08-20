342f18fd7007544926ae78ed484fef8b6749092b
YuliyaShu <juliette7373@gmail.com>
2022-08-20 12:24:40 +0200

    feat: temporary bookPage styles

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import 'normalize.css';
 import App from './controllers/App';
 import state from './models/State';
 import './scss/index.scss';
+import createBookPage from './views/pages/bookPage';
 
 const app = new App(state);
 
 app.start();
+
+const book = createBookPage();
+document.querySelector('body')?.append(book);
