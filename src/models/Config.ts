b15d9e14838f62cbbaff374dedffd1507eff8a3c
Yuliya Shulgan <juliette7373@gmail.com>
2022-08-21 17:11:15 +0200

    Feature/book page (#32)

--- src/models/Config.ts
+++ src/models/Config.ts
@@ -1,3 +1,11 @@
+import section0 from '../assets/images/00-section.jpg';
+import section1 from '../assets/images/01-section.jpg';
+import section2 from '../assets/images/02-section.jpg';
+import section3 from '../assets/images/03-section.jpg';
+import section4 from '../assets/images/04-section.jpg';
+import section5 from '../assets/images/05-section.jpg';
+import section6 from '../assets/images/06-difficult.jpg';
+
 const config = {
   API: {
     URL: 'http://host1836051.hostland.pro',
@@ -13,14 +21,54 @@ const config = {
       },
     },
   },
-  book: {
+
+  BOOK: {
     maxGroup: 6,
     maxPage: 30,
   },
+
   DEFAULT_HEADERS: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
+
+  SECTION_CARD: [
+    {
+      imgUrl: section0,
+      alt: '00-section',
+      sectionName: 'Stage 1 - the easiest',
+    },
+    {
+      imgUrl: section1,
+      alt: '01-section',
+      sectionName: 'Stage 2 - easy',
+    },
+    {
+      imgUrl: section2,
+      alt: '02-section',
+      sectionName: 'Stage 3 - intermediate',
+    },
+    {
+      imgUrl: section3,
+      alt: '03-section',
+      sectionName: 'Stage 4 - upper-intermediate',
+    },
+    {
+      imgUrl: section4,
+      alt: '04-section',
+      sectionName: 'Stage 5 - difficult',
+    },
+    {
+      imgUrl: section5,
+      alt: '05-section',
+      sectionName: 'Stage 7 - the most difficult',
+    },
+    {
+      imgUrl: section6,
+      alt: '06-difficult',
+      sectionName: 'Your challenging words',
+    },
+  ],
 };
 
 export default config;
--- src/views/pages/bookPage.ts
+++ src/views/pages/bookPage.ts
@@ -1,10 +1,46 @@
+import config from '../../models/Config';
+import state from '../../models/State';
 import CustomElement from '../../utils/customElement';
 
 function createBookPage() {
-  const hi = new CustomElement('p', {
-    innerText: 'Hello! I/m a book page!',
+  const mainWrapper = new CustomElement('div', {
+    className: 'main__wrapper book',
   });
-  return hi.element;
+
+  const pageName = new CustomElement('h2', {
+    className: 'book__name',
+    innerText: 'TEXTBOOK',
+  });
+
+  const sectionsWrapper = new CustomElement('div', {
+    className: 'book__sections-wrapper',
+  });
+
+  config.SECTION_CARD.forEach((card) => {
+    const cardWrapper = new CustomElement('div', {
+      className: 'section__wrapper section',
+    });
+
+    if (!state.user?.isAuthorized && card.sectionName === 'Your challenging words') {
+      cardWrapper.element.classList.add('inactive');
+    }
+    const cardImg = new CustomElement('img', {
+      className: 'section__img',
+      src: card.imgUrl,
+      alt: card.alt,
+    });
+    const cardName = new CustomElement('p', {
+      className: 'section_name',
+      innerText: card.sectionName,
+    });
+    cardWrapper.addChildren([cardImg.element, cardName.element]);
+
+    sectionsWrapper.addChildren([cardWrapper.element]);
+  });
+
+  mainWrapper.addChildren([pageName.element, sectionsWrapper.element]);
+
+  return mainWrapper.element;
 }
 
 export default createBookPage;
