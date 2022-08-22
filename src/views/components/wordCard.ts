d4105138e8d6ba1e657893aea9f2431a58b07c0b
YuliyaShu <juliette7373@gmail.com>
2022-08-22 13:58:20 +0200

    feat: section page cards

--- src/views/components/wordCard.ts
+++ src/views/components/wordCard.ts
@@ -4,7 +4,7 @@ import CustomElement from '../../utils/customElement';
 function createWordCard(word: IWord) {
   console.log(word);
   const cardWrapper = new CustomElement('div', {
-    className: 'section__cards-card, card',
+    className: 'section__cards-card card',
   });
   return cardWrapper.element;
 }
