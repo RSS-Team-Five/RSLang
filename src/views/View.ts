5f5488430226a89318d8390384a9f61465b0670a
YuliyaShu <juliette7373@gmail.com>
2022-08-23 11:10:45 +0200

    fix: checking group page

--- src/views/View.ts
+++ src/views/View.ts
@@ -61,7 +61,7 @@ export default class View {
   async renderSection(group: string, page: string) {
     if (this.content) {
       this.content.innerHTML = '';
-      if (+group && +page && +group <= config.BOOK.maxGroup && +page <= config.BOOK.maxPage) {
+      if (+group <= config.BOOK.maxGroup && +page <= config.BOOK.maxPage) {
         const groupAdd = +group as GroupType;
         const pageAdd = +page as PageType;
         const sectionPage: HTMLElement = await createSectionPage(groupAdd, pageAdd);
