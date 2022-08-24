29e9901391c26e1a2035927ee00fd8d7537d2d2e
Bumble <bumble.sakh@gmail.com>
2022-08-24 18:30:19 +1100

    refactor: signUp dialog

--- src/views/components/dialogSignUp.ts
+++ src/views/components/dialogSignUp.ts
@@ -4,25 +4,31 @@ import CustomElement from '../../utils/customElement';
 function dialogSignUp() {
   const dialog = new CustomElement('dialog', { className: 'dialog' });
   const header = new CustomElement('h2', { className: 'dialog__header', textContent: 'Регистрация' });
-  const name = new CustomElement('input', { className: 'dialog__name', type: 'text', placeholder: 'Имя' });
-  const email = new CustomElement('input', { className: 'dialog__email', type: 'text', placeholder: 'Почта' });
-  const password = new CustomElement('input', { className: 'dialog__password', type: 'text', placeholder: 'Пароль' });
-  const btnSignUp = new CustomElement('button', { className: 'dialog__btn', textContent: 'Зарегистрироваться' });
+  const form = new CustomElement('form', { className: 'dialog__form form' });
+  const name = new CustomElement('input', { className: 'form__name', type: 'text', placeholder: 'Имя' });
+  const email = new CustomElement('input', { className: 'form__email', type: 'text', placeholder: 'Почта' });
+  const password = new CustomElement('input', { className: 'form__password', type: 'text', placeholder: 'Пароль' });
+  const btnSignUp = new CustomElement('button', {
+    className: 'form__btn',
+    textContent: 'Зарегистрироваться',
+    type: 'submit',
+  });
   const linkSignIn = new CustomElement('a', {
     className: 'dialog__link',
     href: '#/signIn',
     textContent: 'Уже есть аккаунт? Войти!',
   });
-  dialog.addChildren([
-    header.element,
-    name.element,
-    email.element,
-    password.element,
-    btnSignUp.element,
-    linkSignIn.element,
-  ]);
+  form.addChildren([name.element, email.element, password.element, btnSignUp.element]);
+  dialog.addChildren([header.element, form.element, linkSignIn.element]);
 
-  btnSignUp.element.addEventListener('click', async () => {
+  linkSignIn.element.addEventListener('click', (e) => {
+    e.preventDefault();
+    state.router?.view('/signIn');
+    dialog.element.close();
+  });
+
+  btnSignUp.element.addEventListener('click', async (e) => {
+    e.preventDefault();
     const props = {
       name: name.element.value,
       email: email.element.value,
@@ -39,7 +45,6 @@ function dialogSignUp() {
         localStorage.setItem('refreshToken', resultSignIn.refreshToken);
         state.events?.notify('userAuthorized');
         dialog.element.close();
-        window.history.back();
       } else {
         // Это не должно никогда произойти
         throw new Error('Ошибка авторизации');
