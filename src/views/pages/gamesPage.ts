f0d3f575486d93b367a354069fc8a7665855060d
Bumble <bumble.sakh@gmail.com>
2022-08-28 01:58:59 +1100

    refactor: add game to games page

--- src/views/pages/gamesPage.ts
+++ src/views/pages/gamesPage.ts
@@ -1,10 +1,17 @@
 import CustomElement from '../../utils/customElement';
 
 function createGamesPage() {
+  const wrapper = new CustomElement('div', { className: 'wrapper' });
   const hi = new CustomElement('p', {
     innerText: 'Hello! I/m a games page!',
   });
-  return hi.element;
+  const audioChallenge = new CustomElement('a', {
+    className: 'link',
+    href: '#/games/audio-challenge',
+    innerText: 'Аудио вызов',
+  });
+  wrapper.addChildren([hi.element, audioChallenge.element]);
+  return wrapper.element;
 }
 
 export default createGamesPage;
