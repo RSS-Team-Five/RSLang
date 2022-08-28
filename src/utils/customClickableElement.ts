d39e6c6e2f45f91b45b06cb09f37293bfb5d5c82
YuliyaShu <juliette7373@gmail.com>
2022-08-28 09:52:44 +0200

    refactor: correct CustomClickableElement

--- src/utils/customClickableElement.ts
+++ src/utils/customClickableElement.ts
@@ -1,23 +1,13 @@
 import CustomElement from './customElement';
 
 export default class CustomClickableElement<Tag extends keyof HTMLElementTagNameMap> extends CustomElement<Tag> {
-  element: HTMLElementTagNameMap[Tag];
-
   constructor(
     tagName: Tag,
-    type: keyof WindowEventMap,
-    listener: (event?: Event) => void,
+    event: keyof WindowEventMap,
+    func: EventListener,
     options?: Partial<HTMLElementTagNameMap[Tag]>
   ) {
     super(tagName, options);
-    this.element = document.createElement(tagName);
-    if (options) {
-      Object.assign(this.element, options);
-    }
-    this.element.addEventListener(type, listener);
-  }
-
-  addChildren(children: (Node | string)[]) {
-    super.addChildren(children);
+    this.element.addEventListener(event, func);
   }
 }
