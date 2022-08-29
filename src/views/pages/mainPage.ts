f871430f1c654f132555bc54db4e9732ab747448
YuliyaShu <juliette7373@gmail.com>
2022-08-30 01:18:41 +0200

    feat: main description

--- src/views/pages/mainPage.ts
+++ src/views/pages/mainPage.ts
@@ -6,7 +6,7 @@ function createMainPage() {
   });
 
   const mainFuncCalls = [{ name: 'book' }, { name: 'games' }, { name: 'promo' }, { name: 'statistics' }];
-  const names = [{ name: 'УЧЕБНИК' }, { name: 'ИГРЫ' }, { name: 'О ПРИЛОЖЕНИИ' }, { name: 'СТАТИСТИКА' }];
+  const names = [{ name: 'УЧЕБНИК' }, { name: 'ИГРЫ' }, { name: 'О НАС' }, { name: 'СТАТИСТИКА' }];
 
   mainFuncCalls.map((item, i) => {
     const link = `#/${item.name}`;
@@ -18,6 +18,15 @@ function createMainPage() {
     mainWrapper.addChildren([linkElement.element]);
     return mainWrapper.element;
   });
+
+  const about = new CustomElement('p', {
+    className: 'main__about',
+    innerHTML: `Дорогой друг! Добро пожаловать в невероятно интересную страну английского языка!
+    Здесь ты можешь учиться в УЧЕБНИКЕ на разных уровнях сложности, играть в ИГРАХ, наблюдать за своим ростом в СТАТИСТИКЕ и получать удовольствие от жизни!
+    Рады, что ты с нами! А познакомиться с нами поближе можно на странице О НАС.`,
+  });
+
+  mainWrapper.addChildren([about.element]);
 
   return mainWrapper.element;
 }
