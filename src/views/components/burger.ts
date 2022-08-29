62da51a89809ed9607f92443bcd6042af9f932c4
YuliyaShu <juliette7373@gmail.com>
2022-08-29 23:47:15 +0200

    feat: menu

---
+++ src/views/components/burger.ts
@@ -0,0 +1,41 @@
+import CustomElement from '../../utils/customElement';
+
+function createBurgerMenu(): void {
+  const burgerWrapper = new CustomElement('div', {
+    className: 'burger__wrapper',
+  });
+  // !!! ПОМЕНЯТЬ ИМЯ ВТОРОЙ ИГРЫ
+  const allLinks = [
+    { name: 'book' },
+    { name: 'games/sprint' },
+    { name: 'games/audio-challenge' },
+    { name: 'promo' },
+    { name: 'statistics' },
+  ];
+  const names = [
+    { name: 'УЧЕБНИК' },
+    { name: 'ИГРА СПРИНТ' },
+    { name: 'ИГРА АУДИОВЫЗОВ' },
+    { name: 'О ПРИЛОЖЕНИИ' },
+    { name: 'СТАТИСТИКА' },
+  ];
+
+  allLinks.map((item, i) => {
+    const link = `#/${item.name}`;
+    const linkElement = new CustomElement('a', {
+      className: 'burger__link',
+      href: link,
+      innerText: `${names[i].name}   `,
+    });
+    linkElement.element.addEventListener('click', () => {
+      burgerWrapper.element.style.display = 'none';
+      return burgerWrapper;
+    });
+    burgerWrapper.addChildren([linkElement.element]);
+    return burgerWrapper.element;
+  });
+
+  document.body.prepend(burgerWrapper.element);
+}
+
+export default createBurgerMenu;
--- src/views/components/header.ts
+++ src/views/components/header.ts
@@ -1,5 +1,8 @@
 import state from '../../models/State';
+import CustomClickableElement from '../../utils/customClickableElement';
 import CustomElement from '../../utils/customElement';
+import menuIcon from '../../assets/icons/menu.png';
+import createBurgerMenu from './burger';
 
 function header(): HTMLElement {
   const container = new CustomElement('div', {
@@ -36,12 +39,17 @@ function header(): HTMLElement {
 
   userIconLink.addChildren([userIcon.element]);
 
-  const burger = new CustomElement('div', {
-    className: 'header__burger',
-    innerText: 'future-burger',
+  const eventOnBurger = () => {
+    createBurgerMenu();
+  };
+
+  const burgerIcon = new CustomClickableElement('img', 'click', eventOnBurger, {
+    className: 'header__burger burger',
+    src: menuIcon,
+    alt: 'menu-icon',
   });
 
-  headerWrapper.addChildren([linkToMainPage.element, userIconLink.element, burger.element]);
+  headerWrapper.addChildren([linkToMainPage.element, userIconLink.element, burgerIcon.element]);
   if (state.user?.isAuthorized) {
     const btnSignOut = new CustomElement('button', { className: 'header__sign-in', innerText: 'Выйти' });
     btnSignOut.element.addEventListener('click', () => {
