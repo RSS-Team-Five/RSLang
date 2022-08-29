362718a5080173107ecff807fc39a6864c17a515
YuliyaShu <juliette7373@gmail.com>
2022-08-30 01:26:40 +0200

    feat: start page

--- src/views/pages/aboutUsPage.ts
+++ src/views/pages/aboutUsPage.ts
@@ -1,10 +1,10 @@
 import CustomElement from '../../utils/customElement';
 
-function createPromoPage() {
+function createAboutUsPage() {
   const hi = new CustomElement('p', {
     innerText: 'Hello! I/m a promo page!',
   });
   return hi.element;
 }
 
-export default createPromoPage;
+export default createAboutUsPage;
