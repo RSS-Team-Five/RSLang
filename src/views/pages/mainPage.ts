43c371636beb16982d547d74aa62e7adc27e8360
Yuliya Shulgan <juliette7373@gmail.com>
2022-08-31 11:49:28 +0200

    feat: main description (#62)

--- src/views/pages/mainPage.ts
+++ src/views/pages/mainPage.ts
@@ -7,6 +7,7 @@ function createMainPage() {
   });
 
   const mainFuncCalls = [{ name: 'book' }, { name: 'games' }, { name: 'aboutUs' }, { name: 'statistics' }];
+
   const names = [{ name: 'УЧЕБНИК' }, { name: 'ИГРЫ' }, { name: 'О НАС' }, { name: 'СТАТИСТИКА' }];
 
   mainFuncCalls.map((item, i) => {
@@ -27,6 +28,15 @@ function createMainPage() {
     mainWrapper.addChildren([linkElement.element]);
     return mainWrapper.element;
   });
+
+  const about = new CustomElement('p', {
+    className: 'main__about',
+    innerHTML: `Дорогой друг! Добро пожаловать в невероятно интересную страну английского языка!
+    Здесь ты можешь учиться в УЧЕБНИКЕ на разных уровнях сложности, играть в ИГРАХ, наблюдать за своим ростом в СТАТИСТИКЕ и получать удовольствие от жизни!
+    Рады, что ты с нами! А познакомиться с нами поближе можно на странице О НАС.`,
+  });
+
+  mainWrapper.addChildren([about.element]);
 
   return mainWrapper.element;
 }
