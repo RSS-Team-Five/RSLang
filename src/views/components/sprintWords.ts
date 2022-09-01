60a9eb396dee6c58dc90a3e975449faf18381d7d
muannna <evagoeshard@gmail.com>
2022-09-01 17:43:12 +0500

    feat: get user words for sprint game

--- src/views/components/sprintWords.ts
+++ src/views/components/sprintWords.ts
@@ -1,11 +1,20 @@
 import { wordsFromAPI } from '../../api/words/wordsApi';
 import config from '../../models/Config';
+import state from '../../models/State';
 import IGameWord from '../../types/IGameWord';
 import IWord from '../../types/IWord';
 import shuffle from '../../utils/shuffleArray';
 
 async function getSprintWords(level: number, pageLevel: number | undefined) {
   const pagesSet = new Set<number>();
+  let userLearnedWords: IWord[] = [];
+
+  if (state.user?.isAuthorized && pageLevel !== undefined) {
+    const words = await state.user.getUserAggregatedWords(state.user.user, {
+      filter: { 'userWord.optional.learned': true },
+    });
+    userLearnedWords = words[0].paginatedResults;
+  }
 
   if (pageLevel === undefined) {
     while (pagesSet.size < 3) {
@@ -26,16 +35,20 @@ async function getSprintWords(level: number, pageLevel: number | undefined) {
 
   await Promise.all(
     pagesArr.map(async (page) => {
-      const wordsFromPage = (await wordsFromAPI(level, page)) as IWord[];
+      let wordsFromPage = (await wordsFromAPI(level, page)) as IWord[];
+      if (userLearnedWords.length > 0) {
+        userLearnedWords.forEach((word) => {
+          wordsFromPage = wordsFromPage.filter((e) => e.word !== word.word);
+        });
+      }
       wordsArray = wordsArray.concat(wordsFromPage);
     })
   );
 
-  console.log(wordsArray);
   wordsArray = shuffle(wordsArray);
 
-  const correctWords = JSON.parse(JSON.stringify(wordsArray.slice(0, wordsArray.length / 2 + 1))) as IGameWord[];
-  const incorrectWords = JSON.parse(JSON.stringify(wordsArray.slice(wordsArray.length / 2 + 1))) as IGameWord[];
+  const correctWords = JSON.parse(JSON.stringify(wordsArray.slice(0, wordsArray.length / 2))) as IGameWord[];
+  const incorrectWords = JSON.parse(JSON.stringify(wordsArray.slice(wordsArray.length / 2))) as IGameWord[];
 
   incorrectWords.forEach((word) => {
     const randomIndex = Math.floor(Math.random() * wordsArray.length);
