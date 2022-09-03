c9a5f2db4737514ce39af8cf17ec647db19c051a
Bumble <bumble.sakh@gmail.com>
2022-09-04 06:40:00 +1100

    feat: add statistic styles

---
+++ src/views/components/outerBall.ts
@@ -0,0 +1,25 @@
+import CustomElement from '../../utils/customElement';
+import arrow from '../../assets/icons/Arrow 1.png';
+
+export default function getOuterBall(index: number) {
+  const outerBall = new CustomElement('div', {
+    className: `outer-ball${index}`,
+  });
+
+  const InnerBall = new CustomElement('div', {
+    className: `inner-ball${index}_first`,
+  });
+
+  const secondInnerBall = new CustomElement('div', {
+    className: `inner-ball${index}_second`,
+  });
+
+  const innerArrow = new CustomElement('img', {
+    className: `inner-arrow${index}`,
+    src: arrow,
+    alt: 'arrow-icon',
+  });
+
+  outerBall.addChildren([InnerBall.element, secondInnerBall.element, innerArrow.element]);
+  return outerBall.element;
+}
