73cd5304e5717b7d8aa88dbdf2b6060c71a844e7
muannna <evagoeshard@gmail.com>
2022-09-05 08:28:30 +0500

    feat: add full audio challenge styles

--- src/views/AudioChallengeView.ts
+++ src/views/AudioChallengeView.ts
@@ -5,6 +5,8 @@ import AudioChallengeModel from '../models/AudioChallengeModel';
 import config from '../models/Config';
 import sound from '../assets/icons/GREEN-sound.png';
 import getOuterBall from './components/outerBall';
+import IWord from '../types/IWord';
+import drawChart from './components/audioChart';
 
 export default class AudioChallengeView {
   controller: AudioChallengeController;
@@ -30,8 +32,8 @@ export default class AudioChallengeView {
     const line = new CustomElement('span', {
       className: 'ac__line',
     });
-    const acBall = getOuterBall(1);
-    acBall.classList.add('bottom');
+    const acBall = getOuterBall(2);
+    acBall.classList.add('ac__bottom');
     this.view.addChildren([gameContainer.element, line.element, acBall]);
     if (model.words && model.answers) {
       const word = model.words[model.currentWord];
@@ -109,27 +111,84 @@ export default class AudioChallengeView {
 
   renderResult(model: AudioChallengeModel) {
     this.view.element.innerHTML = '';
-    const gameContainer = new CustomElement('div', { className: 'game__container__ac' });
-    this.view.addChildren([gameContainer.element]);
-    const result = new CustomElement('div', {
-      innerText: `SCORE - ${model.gameStatistic.score}\nSERIES - ${model.gameStatistic.winSeries}\n`,
-    });
-    const win = new CustomElement('div', { innerText: `WIN - ${model.gameStatistic.win.length}\n` });
-    model.gameStatistic.win.forEach((word) => {
-      const row = new CustomElement('p', { innerText: `${word.word} - ${word.wordTranslate}` });
-      win.addChildren([row.element]);
-    });
-    const lose = new CustomElement('div', { innerText: `LOSE - ${model.gameStatistic.lose.length}\n` });
-    model.gameStatistic.lose.forEach((word) => {
-      const row = new CustomElement('p', { innerText: `${word.word} - ${word.wordTranslate}` });
-      lose.addChildren([row.element]);
-    });
-    result.addChildren([win.element, lose.element]);
+    const resultTable = new CustomElement('div', { className: 'game__result__table' });
 
-    const mainPageLink = new CustomElement('a', { href: '#/', innerText: 'На главную' });
-    const newGameLink = new CustomElement('a', { href: '#/games/audio-challenge', innerText: 'Играть заново' });
+    const line = new CustomElement('span', { className: 'ac__line' });
+    const acBall = getOuterBall(2);
+    acBall.classList.add('ac__bottom');
+    this.view.addChildren([resultTable.element, line.element, acBall]);
 
-    gameContainer.addChildren([result.element, mainPageLink.element, newGameLink.element]);
+    const resultHeader = new CustomElement('div', { className: 'result__table__header' });
+    const resultTitle = new CustomElement('h2', {
+      className: 'header__title',
+      textContent: 'Результаты'.toUpperCase(),
+    });
+    const resultAgainButton = new CustomElement('a', {
+      className: 'header__again-button',
+      textContent: 'Повторить'.toUpperCase(),
+      href: '#/games/audio-challenge',
+    });
+    resultHeader.addChildren([resultTitle.element, resultAgainButton.element]);
+    const resultMain = new CustomElement('div', { className: 'result__table__main' });
+    resultTable.addChildren([resultHeader.element, resultMain.element]);
+    const resultTotalScore = new CustomElement('div', { className: 'game__result__total-score' });
+    const resultScoreFirst = new CustomElement('p', {
+      className: 'total-score__text',
+      textContent: 'Вы набрали '.toUpperCase(),
+    });
+    const resultScorePoints = new CustomElement('p', {
+      innerText: `${model.gameStatistic.score}`,
+      className: 'total-score__points',
+    });
+    const resultScoreSecond = new CustomElement('p', {
+      className: 'total-score__text',
+      textContent: ' баллов'.toUpperCase(),
+    });
+    const resultDiagram = new CustomElement('div', { className: 'game__result__diagram' });
+    const diagramChart = new CustomElement('canvas', { className: 'diagram_chart' });
+    resultTotalScore.addChildren([resultScoreFirst.element, resultScorePoints.element, resultScoreSecond.element]);
+    resultDiagram.addChildren([diagramChart.element]);
+    drawChart(model, diagramChart);
+
+    const points = new CustomElement('div', { className: 'game__result__points' });
+    for (let i = 0; i < 3; i += 1) {
+      const dotProgress = new CustomElement('div', {
+        className: `game__result__points_dot${i}`,
+      });
+      points.addChildren([dotProgress.element]);
+    }
+    resultMain.addChildren([resultTotalScore.element, resultDiagram.element, points.element]);
+    const correctTitle = new CustomElement('div', {
+      className: 'game__result__title_cor',
+      textContent: 'верно'.toUpperCase(),
+    });
+    resultTable.addChildren([correctTitle.element]);
+    model.gameStatistic.win.forEach((word) => AudioChallengeView.drawWords(word, 'win', resultTable));
+    const incorrectTitle = new CustomElement('div', {
+      className: 'game__result__title_incor',
+      textContent: 'ошибки'.toUpperCase(),
+    });
+    resultTable.addChildren([incorrectTitle.element]);
+    model.gameStatistic.lose.forEach((word) => AudioChallengeView.drawWords(word, 'lose', resultTable));
+  }
+
+  static drawWords(word: IWord, param: string, resultTable: CustomElement<'div'>) {
+    const wordLine = new CustomElement('div', { className: 'game__result__word-line' });
+    const correctness = new CustomElement('div', {});
+    if (param === 'win') {
+      correctness.element.classList.add('game__result__points_dot2');
+    } else {
+      correctness.element.classList.add('game__result__points_dot1');
+    }
+    const wordName = new CustomElement('p', { className: 'game__result__word', textContent: word.word.toUpperCase() });
+    const wordDash = new CustomElement('p', { className: 'game__result__word_dash', textContent: '-' });
+    const wordTranslation = new CustomElement('p', {
+      className: 'game__result__word_translation',
+      textContent: word.wordTranslate.toUpperCase(),
+    });
+
+    wordLine.addChildren([correctness.element, wordName.element, wordDash.element, wordTranslation.element]);
+    resultTable.addChildren([wordLine.element]);
   }
 
   renderNoWords() {
